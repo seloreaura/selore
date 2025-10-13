94972364ba26c740f86fec77423edb2b98801482
seloreaura <aloreaura25@gmail.com>
2025-10-13 17:34:06 -0300

    delete src

--- src/types/routes.ts
+++
@@ -1,6 +0,0 @@
-import { RouteObject } from 'react-router-dom';
-
-export interface CustomRouteObject extends Omit<RouteObject, 'children'> {
-  title?: string;
-  children?: CustomRouteObject[];
-}
